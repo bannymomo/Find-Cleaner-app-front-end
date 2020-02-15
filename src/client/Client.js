686c82328a5d88a1c71fb71b4bae86e9c8e8d823
Vincent Li <vincent0lee95@gmail.com>
2020-02-15 16:22:44 +1000

    client info (unfinished)

---
+++ src/client/Client.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import './style/client.scss'
+
+class Client extends Component {
+
+    render() {
+        return (
+            <div className='container'>
+
+            </div>
+            
+        )
+    }
+}
+
+export default Client;
