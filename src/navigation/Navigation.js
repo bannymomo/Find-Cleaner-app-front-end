a3003fa421c8f5aab2fce1d2ca15a58655a11e53
Vincent0Lee <vincent0lee95@gmail.com>
2020-02-18 18:41:28 +1000

    Add log in

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
+import { Button } from '@material-ui/core'
+import { NavLink } from 'react-router-dom';
+
+import { LOGIN_URL } from '../routes/URLMap';
 import "./style/navigation.scss";
 import cleaner from "../assets/images/cleaner.png";
+
 class Navigation extends Component {
   render() {
     return (
       <header className="navigation">
         <img src={cleaner} alt="cleaner" />
         <button className="post">Post a task</button>
+        <Button color="primary">
+          <NavLink to={LOGIN_URL} style={{ textDecoration: 'none' }}>
+            Log in
+          </NavLink>
+        </Button>
+        
         <button className="avatar"></button>
+
       </header>
     );
   }
