a3b5c928ed7016f8cf277d13b6364d7c06258741
bannymomo <bannymomo33@gmail.com>
2020-02-20 11:35:46 +1000

    run test and fix problems

--- src/homepage/components/AppBar.js
+++ src/homepage/components/AppBar.js
@@ -1,30 +1,30 @@
 import React from "react";
-import PropTypes from "prop-types";
 import MuiAppBar from "@material-ui/core/AppBar";
 import "../../theme/color.scss";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#2196f3"
-    },
-    secondary: {
-      main: "#f50057"
-    }
-  }
+	palette: {
+		primary: {
+			main: "#2196f3"
+		},
+		secondary: {
+			main: "#f50057"
+		}
+	}
 });
 
 function AppBar(props) {
-  return (
-    <ThemeProvider theme={theme}>
-      <MuiAppBar color="primary" elevation={0} position="static" {...props} />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<MuiAppBar
+				color="primary"
+				elevation={0}
+				position="static"
+				{...props}
+			/>
+		</ThemeProvider>
+	);
 }
-
-AppBar.propTypes = {
-  classes: PropTypes.object.isRequired
-};
 
 export default AppBar;
