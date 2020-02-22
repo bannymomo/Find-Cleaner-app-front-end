3ec7ea0cd69ad076169cd5a57100be1b0f34ec8f
leah Li <leah.li8125@gmail.com>
2020-02-22 15:32:25 +1000

    browse order:top bar-tasksorder,newTask,serach

--- src/business/BrowseOrder/BrowseOrder.js
+++ src/business/BrowseOrder/BrowseOrder.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import SearchBar from "./components/Search";
+import DatePosted from "./components/DatePosted";
+import NewTasks from "./components/NewTasks";
+import "./style/browseorders.scss";
+import { Divider } from "@material-ui/core";
 
 class BrowseOrder extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Browse Order</h1>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<div className="browse-orders--top-bar">
+					<DatePosted />
+					<NewTasks />
+					<SearchBar />
+				</div>
+				<Divider />
+			</div>
+		);
+	}
 }
 
 export default BrowseOrder;
--- src/business/SideBar.js
+++ src/business/SideBar.js
@@ -9,65 +9,88 @@ import Collapse from "@material-ui/core/Collapse";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    width: "100%",
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper
-  }
+	root: {
+		width: "100%",
+		maxWidth: 360,
+		backgroundColor: theme.palette.background.paper
+	},
+	tasksButton: {
+		margin: "0.3rem 0.5rem",
+		borderRadius: "100px",
+		padding: "0.7rem 1.6rem",
+		fontSize: "0.9rem",
+		letterSpacing: "1px",
+		fontWeight: "700"
+	}
 }));
 
 function ListItemLink(props) {
-  return <ListItem button component={Link} {...props} />;
+	return <ListItem button component={Link} {...props} />;
 }
 
 export default function SimpleList() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const [open, setOpen] = React.useState(true);
+	const [open, setOpen] = React.useState(true);
 
-  const handleClick = () => {
-    setOpen(!open);
-  };
+	const handleClick = () => {
+		setOpen(!open);
+	};
 
-  return (
-    <div>
-      <div className="client__avatar-container--left-top">
-        <Avatar />
-        <p>business name</p>
-      </div>
-      <div className={classes.root}>
-        <Divider />
-        <List component="nav" aria-label="secondary mailbox folders">
-          <ListItemLink to="dashboard">
-            <ListItemText primary="Dashboard" />
-          </ListItemLink>
-          <ListItemLink to="profile">
-            <ListItemText primary="Profile" />
-          </ListItemLink>
-          <ListItemLink to="browse-order">
-            <ListItemText primary="Take Orders" />
-          </ListItemLink>
-          <ListItemLink to="order-history">
-            <ListItemText primary="OrderHistory" />
-          </ListItemLink>
-          <ListItem button onClick={handleClick}>
-            <ListItemText primary="Setting" />
-            {open ? <ExpandLess /> : <ExpandMore />}
-          </ListItem>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <List component="div">
-              <ListItem className={classes.nested}>
-                <ListItemLink to="account">Account</ListItemLink>
-              </ListItem>
-              <ListItem className={classes.nested}>
-                <ListItemLink to="password">Password</ListItemLink>
-              </ListItem>
-            </List>
-          </Collapse>
-        </List>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="client__avatar-container--left-top">
+				<Avatar />
+				<p>business name</p>
+			</div>
+			<div className={classes.root}>
+				<Divider />
+
+				<List component="nav" aria-label="secondary mailbox folders">
+					<ListItemLink to="browse-order">
+						<Button
+							className={classes.tasksButton}
+							variant="contained"
+							type="button"
+							color="secondary"
+							to="browse-order"
+						>
+							Browser Tasks
+						</Button>
+					</ListItemLink>
+					<ListItemLink to="dashboard">
+						<ListItemText primary="Dashboard" />
+					</ListItemLink>
+					<ListItemLink to="profile">
+						<ListItemText primary="Profile" />
+					</ListItemLink>
+
+					<ListItemLink to="order-history">
+						<ListItemText primary="OrderHistory" />
+					</ListItemLink>
+					<ListItem button onClick={handleClick}>
+						<ListItemText primary="Setting" />
+						{open ? <ExpandLess /> : <ExpandMore />}
+					</ListItem>
+					<Collapse in={open} timeout="auto" unmountOnExit>
+						<List component="div">
+							<ListItem className={classes.nested}>
+								<ListItemLink to="account">
+									Account
+								</ListItemLink>
+							</ListItem>
+							<ListItem className={classes.nested}>
+								<ListItemLink to="password">
+									Password
+								</ListItemLink>
+							</ListItem>
+						</List>
+					</Collapse>
+				</List>
+			</div>
+		</div>
+	);
 }
