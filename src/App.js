272030b39670f37f4406c581577453cf6a76c3af
bannymomo <bannymomo33@gmail.com>
2020-02-24 20:52:27 +1000

    change margin-top to business and client

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import Routes from "./routes/Routes";
 import "./App.scss";
 
 function App() {
-  return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <div className="App" style={{ backgroundColor: "#f5f6fd" }}>
-        <Navigation />
-        <main className="App__container--whole-page">
-          <Routes />
-        </main>
-      </div>
-    </MuiPickersUtilsProvider>
-  );
+	return (
+		<MuiPickersUtilsProvider utils={DateFnsUtils}>
+			<Navigation />
+			<div className="App" style={{ backgroundColor: "#f5f6fd" }}>
+				<main className="App__container--whole-page">
+					<Routes />
+				</main>
+			</div>
+		</MuiPickersUtilsProvider>
+	);
 }
 
 export default App;
