4c76cf8e4b119fc6aa251977b9fff66f1069dcfd
Skye <wym0529@hotmail.com>
2020-03-16 15:17:20 +1000

    rename folders

--- src/client/Client.js
+++ src/client/Client.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import "./style/client.scss";
-import SideBar from "./sidebar/SideBar";
+import SideBar from "./sideBar/SideBar";
 import ClientRoutes from "../routes/ClientRoutes";
 import TemporaryDrawer from "./TemporaryDrawer";
 import DashboardNavigation from "../navigation/DashboardNavigation";
