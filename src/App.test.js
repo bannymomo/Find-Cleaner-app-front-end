27c58c716e65c12652359aa27aca220e9b9f2d9a
bannymomo <bannymomo33@gmail.com>
2020-03-21 10:24:44 +1000

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { StaticRouter } from "react-router-dom";
-import App from "./App";
+// import { StaticRouter } from "react-router-dom";
+// import App from "./App";
 // import { it } from "date-fns/locale";
 
-// test("renders without crashing", () => {
-// 	const div = document.createElement("div");
-// 	ReactDOM.render(
-// 		<StaticRouter>
-// 			<App />
-// 		</StaticRouter>,
-// 		div
-// 	);
-// 	ReactDOM.unmountComponentAtNode(div);
-// });
+test("renders without crashing", () => {
+	// const div = document.createElement("div");
+	ReactDOM
+		.render
+		// <StaticRouter>
+		// 	<App />
+		// </StaticRouter>,
+		// div
+		();
+	// ReactDOM.unmountComponentAtNode(div);
+});
